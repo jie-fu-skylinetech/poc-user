817bfd34e49e42bc93afac66b9a5225ebdec1828
jie.fu.skylinetech <33968725+jie-fu-skylinetech@users.noreply.github.com>
2017-12-22 14:13:27 -0500

    Update user.py

--- gcp/user.py
+++ gcp/user.py
@@ -14,9 +14,9 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 # [END create_app]
 
 def saveImage(imageId, imageData):
-	client = storage.Client()
-	bucket = client.get_bucket('user-images')
-	blob = bucket.blob("./images/{}.jpg".format(imageId))
+    client = storage.Client()
+    bucket = client.get_bucket('user-images')
+    blob = bucket.blob("./images/{}.jpg".format(imageId))
     blob.upload_from_string(imageData, 'image/jpeg')
 #    os.mkdir('images')
 #    with open("./images/{}.jpg".format(imageId), 'w+b') as selfie:
